d25d39a415659240f31c556780b31c6686d9c268
Eixix <tobiasbetz@hotmail.de>
2023-03-09 15:32:17 +0100

    Added simple api wrapper

--- src/App.vue
+++ src/App.vue
@@ -9,10 +9,25 @@ import { apiGetRequest } from './helpers/apiHelper'
 
 <script>
 export default {
-  async mounted() {
-    const response = await apiGetRequest('config')
+  data() {
+    return { timeslots: 0 }
+  },
 
-    console.log(response)
+  methods: {
+    async checkForUpdates() {
+      const now = new Date().toISOString()
+      const nextWeek = new Date(new Date().setDate(new Date().getDate() + 7)).toISOString()
+      const response = await apiGetRequest(`calendars/calendar.uni?start=${now}&end=${nextWeek}`)
+      if (this.timeslots === 0) {
+        this.timeslots = response.length
+      } else if (this.timeslots !== response.length) {
+        console.log('Neuer Termin!')
+      }
+    }
+  },
+
+  mounted() {
+    setInterval(this.checkForUpdates, 2000)
   }
 }
 </script>
