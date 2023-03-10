663a8e17aaf310497ea2bd96e7a684eb9bd08d20
Eixix <tobiasbetz@hotmail.de>
2023-03-10 09:28:51 +0100

    Removed polling, added websockets

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-import { apiGetRequest } from './helpers/apiHelper'
 </script>
 
 <template>
@@ -10,24 +9,62 @@ import { apiGetRequest } from './helpers/apiHelper'
 <script>
 export default {
   data() {
-    return { timeslots: 0 }
+    return {
+      connection: null,
+      connectionId: 1,
+      connectionStates: { disconnected: 'disconnected', connected: 'connected' },
+      connectionState: 'disconnected'
+    }
   },
 
-  methods: {
-    async checkForUpdates() {
-      const now = new Date().toISOString()
-      const nextWeek = new Date(new Date().setDate(new Date().getDate() + 7)).toISOString()
-      const response = await apiGetRequest(`calendars/calendar.uni?start=${now}&end=${nextWeek}`)
-      if (this.timeslots === 0) {
-        this.timeslots = response.length
-      } else if (this.timeslots !== response.length) {
-        console.log('Neuer Termin!')
+  methods: {},
+
+  created() {
+    this.connection = new WebSocket('ws://hass.home/api/websocket')
+
+    this.connection.onmessage = (event) => {
+      const data = JSON.parse(event.data)
+      switch (data.type) {
+        case 'auth_required':
+          console.log('Starte Authentifizierung...')
+          this.connection.send(
+            JSON.stringify({
+              type: 'auth',
+              access_token: import.meta.env.VITE_HOMEASSISTANT_TOKEN
+            })
+          )
+          break
+        case 'auth_ok':
+          console.log('Authentifizierung abgeschlossen!')
+          this.connectionState = this.connectionStates.connected
+          this.connection.send(
+            JSON.stringify({
+              id: this.connectionId,
+              type: 'subscribe_events'
+            })
+          )
+          this.connectionId++
+          break
+        case 'auth_invalid':
+          alert('Generiere ein neues Token, dein altes ist ungültig')
+          this.connectionState = this.connectionStates.disconnected
+          break
+        case 'event':
+          console.log(data)
+          break
+        case 'result':
+          console.log(data)
+          break
       }
+    }
+
+    this.connection.onopen = function (event) {
+      console.log('Successfully connected to the echo websocket server...')
     }
   },
 
   mounted() {
-    setInterval(this.checkForUpdates, 2000)
+    //setInterval(this.checkForUpdates, 2000)
   }
 }
 </script>
