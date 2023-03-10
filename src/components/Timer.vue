69e3789ee1cd306349169514c3dd0ca0a95bc7f4
Eixix <tobiasbetz@hotmail.de>
2023-03-10 10:46:45 +0100

    Sort of spawning timers

---
+++ src/components/Timer.vue
@@ -0,0 +1,87 @@
+<template>
+  <div class="timer">
+    <svg class="timer-svg" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+      <g class="timer-circle">
+        <circle class="time-elapsed-path" cx="50" cy="50" r="45" />
+      </g>
+    </svg>
+    <div class="time-left-container">
+      <span class="time-left-label">{{ timeLeftString }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return { timerInterval: null, timeLeft: 0 }
+  },
+
+  computed: {
+    timeLeftString() {
+      const timeLeft = this.timeLeft
+      const minutes = Math.floor(timeLeft / 60)
+      const seconds = timeLeft % 60
+      return `${this.padToTwo(minutes)}:${this.padToTwo(seconds)}`
+    }
+  },
+
+  props: {
+    limit: {
+      type: Number,
+      required: true
+    }
+  },
+
+  methods: {
+    padToTwo(num) {
+      return String(num).padStart(2, '0')
+    }
+  },
+
+  mounted() {
+    this.timeLeft = this.limit
+    this.timerInterval = setInterval(() => {
+      if (--this.timeLeft === 0) {
+        clearInterval(this.timerInterval)
+      }
+    }, 1000)
+  }
+}
+</script>
+
+<style scoped>
+.timer {
+  height: 300px;
+  width: 300px;
+  position: relative;
+}
+
+.timer-circle {
+  fill: none;
+  stroke: none;
+}
+
+.time-elapsed-path {
+  stroke-width: 7px;
+  stroke: #424242;
+}
+
+.time-left-container {
+  height: inherit;
+  width: inherit;
+
+  position: absolute;
+  top: 0;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.time-left-label {
+  font-size: 50px;
+  font-family: 'Segoe UI';
+  color: white;
+}
+</style>
